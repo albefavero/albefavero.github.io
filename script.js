18fbff4492c34903d25dc426158e4aae237a85bd
albefavero <145295258+albefavero@users.noreply.github.com>
2023-09-18 19:32:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+import sentences from './sentences.js';
+
+const randomSentenceButton = document.getElementById('randomSentenceButton');
+const randomSentenceDisplay = document.getElementById('randomSentence');
+
+randomSentenceButton.addEventListener('click', () => {
+  const randomIndex = Math.floor(Math.random() * sentences.length);
+  const randomSentence = sentences[randomIndex];
+  randomSentenceDisplay.innerText = randomSentence;
+});
