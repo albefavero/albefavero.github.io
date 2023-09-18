e44ff896be77aeca096ffcec0618b053b0d06c71
albefavero <145295258+albefavero@users.noreply.github.com>
2023-09-19 00:27:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -376,4 +376,21 @@ randomSentenceButton.addEventListener('click', () => {
   const randomIndex = Math.floor(Math.random() * sentences.length);
   const randomSentence = sentences[randomIndex];
   randomSentenceDisplay.innerText = randomSentence;
+
+const downloadButton = document.getElementById('downloadButton');
+downloadButton.addEventListener('click', downloadSentences);
+
+function downloadSentences() {
+  const content = sentences.join('\n');  // Join sentences with line breaks
+  const blob = new Blob([content], { type: 'text/plain' });
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = 'frasi.txt';  // Set the desired file name
+  a.click();
+
+  URL.revokeObjectURL(url);
+}
+  
 });
